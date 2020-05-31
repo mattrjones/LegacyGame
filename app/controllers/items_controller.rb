959c366d9aa93c283d60c860695593a85c8a70bd
Matthew Jones <matts3371@gmail.com>
2020-05-31 11:47:10 -0500

    Added logic to make sure an upgrade could be performed with a flash warning

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,12 @@ class ItemsController < ApplicationController
 
     def upgrade
         @item = Item.find_by(params[:id])
+        @hero = Hero.find(1)
+        if @hero.exp_total < @item.upg_exp_cost
+            redirect_to "/encounters/blacksmith", notice: "You don't have enough exp to do this."
+        elsif @hero.exp_total >= @item.upg_exp_cost
+            render 'upgrade' 
+        end
     end 
 
     def show 
